4fad44dd1501eee1d26f9aff35fbe7fca00f3837
Leo <yh821@qq.com>
2018-10-24 21:50:36 +0800

    ShaderForge

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour 
+{
+	public float speed = 5f;
+	public Vector3 axis = Vector3.right;
+	public int angleMod = 360;
+
+	float angle=50;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += Time.deltaTime*speed;
+		angle = angle%angleMod;
+		transform.rotation = Quaternion.AngleAxis (angle,axis);
+	}
+}
